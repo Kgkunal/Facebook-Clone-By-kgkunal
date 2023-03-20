115d3a307c2d36ff1376a67079e9d47f079a5968
Kunal D Gavhane <kgkunalgavhane1721@gmail.com>
2023-03-21 02:25:29 +0530

    solved some errors regrading php script

--- sign.php
+++ sign.php
@@ -11,7 +11,7 @@ if( isset($_POST['first-name']) && !empty($_POST['first-name'])){
     $birthMonth= $_POST['birth-month'];
     $birthYear= $_POST['birth-year'];
     $upgen= $_POST['gen'];
-    $birth= $_POST[''.birthYear.'-'.birthMonth.''.birthDay.''];
+    $birth= $_POST[' '.$birthYear.'-'.$birthMonth.'-'.$birthDay.' '];
 
     echo "$birth";
 
@@ -87,35 +87,35 @@ if( isset($_POST['first-name']) && !empty($_POST['first-name'])){
 
     <script src="assets/js/jquery.js"></script>
     <script>
-        for(i= new Date().getFullYear() ; i > 1900 ; i--){
-
-          $("#years").append($('<option/>').val(i).html(i));
+        for (i = new Date().getFullYear(); i > 1900; i--) {
+            //    2019,2018, 2017,2016.....1901
+            $("#years").append($('<option/>').val(i).html(i));
 
         }
-            for(i=1; i < 13 ; i++){
-               $('#months').append($('<option/>').val(i).html(i));
+        for (i = 1; i < 13; i++) {
+            $('#months').append($('<option/>').val(i).html(i));
+        }
+        updateNumberOfDays();
+
+        function updateNumberOfDays() {
+            $('#days').html('');
+            month = $('#months').val();
+            year = $('#years').val();
+            days = daysInMonth(month, year);
+            for (i = 1; i < days + 1; i++) {
+                $('#days').append($('<option/>').val(i).html(i));
             }
 
+        }
+        $('#years, #months').on('change', function() {
             updateNumberOfDays();
+        })
 
-            function updateNumberOfDays(){
-                $('#days').html('');
-                month= $('#months').val();
-                year= $('#years').val();
-                days = daysInMonth(month, year);
-                
-                for(i=1; i < days+1 ; i++){
-                    $('#days').append($('<option/>').val(i).html(i));
-                }
-            }
+        function daysInMonth(month, year) {
+            return new Date(year, month, 0).getDate();
 
-            $('#days , #months').on('change' , function(){
-                updateNumberOfDays();
-            })
+        }
 
-            function daysInMonth(month,year){
-                return new Date(year, month, 0).getDate();
-            }
     </script>
 </body>
 </html>
